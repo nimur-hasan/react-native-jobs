812c0b73d0a6165428619c39686ea70f49a97187
nimur-hasan <developernaim20@gmail.com>
2023-06-02 17:07:45 +0600

    Ready for implement api

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,21 @@
-import { Stack } from 'expo-router'
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router'
+import { useCallback } from 'react';
 
 const Layout = () => {
-    return <Layout/>
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMBoldRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async() => {
+        if(fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    },  [fontsLoaded])
+
+    return <Stack onLayout={onLayoutRootView} />
 }
+
+export default Layout;
---
+++ components/home/popular/Popularjobs.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { View, Text, ActivityIndicator } from 'react-native'
+
+import styles from './popularjobs.style'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { COLORS, SIZES } from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+
+const Popularjobs = () => {
+  isLoading = false;
+  error = false
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>     
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size={'large'} color={COLORS.primary}/>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList 
+            data={[1,2,3,4,5]}
+            renderItem={({item}) => (
+              <PopularJobCard
+                item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{columnGap: SIZES.medium}}
+            horizontal
+          />
+        )
+        }
+      </View> 
+    </View>
+  )
+}
+
+export default Popularjobs
